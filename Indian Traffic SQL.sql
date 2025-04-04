27531f7c4da27cab52126eed25cc386aad8e481d
Okolie Augustina Chioma <138698620+Azdahyu@users.noreply.github.com>
2025-04-04 21:56:46 +0100

    Update Indian Traffic SQL.sql

--- Indian Traffic SQL.sql
+++ Indian Traffic SQL.sql
@@ -36,8 +36,7 @@ HAVING COUNT(*) > 1;
 ----------------------------------------------------------------------------------------------------------------------
 
 /*
-Check for blanks and null values
-None of the following queries returned a null or blank value
+Check for blanks and null values in each column (there were no null or blank values)
 */
 
 SELECT *
@@ -118,116 +117,140 @@ WHERE Previous_Violations = '' OR NULL;
 
 ---------------------------------------------------------------------------------------------------------------------
 
-/*
-Return each column with the number of violations
-*/
+--What are the types of violation and how many times are they committed?
 
 SELECT Violation_Type, COUNT(Violation_Type) AS num_of_violations
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER BY 2 DESC;
 
+--At what locations do these violations occur and how many violatioins were recored for each location?
+
 SELECT Location, COUNT(Violation_Type) AS num_of_violations
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER BY 2 DESC;
+
+--What are the vehicle types and many violations are recorded for each type?
 
 SELECT Vehicle_Type, COUNT(Violation_Type) AS num_of_violations
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER  BY 2 DESC;
 
+--What is the range of the drivers age and how many violations are recorded for each age?
+
 SELECT Driver_Age, COUNT(Violation_Type) AS num_of_violations
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER  BY 2 DESC;
+
+--What are the drivers gender and how many violations has each gender committed?
 
 SELECT Driver_Gender, COUNT(Violation_Type) AS num_of_violations
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER  BY 2 DESC;
 
+--What are the license types and how many violations are recorded for each type?
+
 SELECT License_Type, COUNT(Violation_Type) AS num_of_violations
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER  BY 2 DESC;
+
+--What is the average penalty point for each violation type?
 
 SELECT Violation_Type, AVG(Penalty_Points) AS avg_penalty_points
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER  BY 2 DESC;
 
+--What was the weather condition when the violation was committed and how many times has these violations been committed during those weather conditions?
+
 SELECT Weather_Condition, COUNT(Violation_Type) AS num_of_violations
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER  BY 2 DESC;
+
+--What was the road condition when these violations were committed and how many violations are recorded for each road condition?
 
 SELECT Road_Condition, COUNT(Violation_Type) AS num_of_violations
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER  BY 2 DESC;
 
+--What issuing agency handled these violations and how many times has each agency handled a violation case?
+
 SELECT Issuing_Agency, COUNT(Violation_Type) AS num_of_violations
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER  BY 2 DESC;
+
+--What was the license validity of the drivers and many violations are recorded for each validity type?
 
 SELECT License_Validity, COUNT(Violation_Type) AS num_of_violations
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER  BY 2 DESC;
 
+--What was the status of the traffic light when these violations were committed and how many violations were recorded for each status?
+
 SELECT Traffic_Light_Status, COUNT(Violation_Type) AS num_of_violations
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER  BY 2 DESC;
+
+--What were the recorded speed and how many violations fall under each speed?
 
 SELECT Recorded_Speed, COUNT(Violation_Type) AS num_of_violations
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER  BY 2 DESC;
 
+--What were the alcohol level of the offenders and how many violations were recorded for each alcohol level?
+
 SELECT Alcohol_Level, COUNT(Violation_Type) AS num_of_violations
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER  BY 2 DESC;
+
+--What were the breath analyzer results and how many times was it conducted for each result?
 
 SELECT Breathalyzer_Result, COUNT(Violation_Type) AS num_of_violations
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER  BY 2 DESC;
 
+--Did the offenders pay their fine? How many paid or did not pay?
+
 SELECT Fine_Paid, COUNT(Violation_Type) AS num_of_violations
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER  BY 2 DESC;
+
+--Was it necessary for the offenders to appear in court and how many of them had to appear or not appear?
 
 SELECT Court_Appearance_Required, COUNT(Violation_Type) AS num_of_violations
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER  BY 2 DESC;
 
+--How many previous violations did the drivers have and how many more violations have they committed?
+
 SELECT Previous_Violations, COUNT(Violation_Type) AS num_of_violations
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER  BY 2 DESC;
 
---------------------------------------------------------------------------------------------------------------------
-
-/*
-Return the minimum and maximum fine amount for each violation type
-*/
+--What were the maximum and minimum fine amount for each violation type?
 
 SELECT Violation_Type, MIN(Fine_Amount) min_fine, MAX(Fine_Amount) max_fine
 FROM indian_traffic_violations1
 GROUP BY 1
 ORDER  BY 2 DESC;
 
--------------------------------------------------------------------------------------------------------------------
-
-/*
-Return the average fine amount for each violation type
-*/
+--What was the average fine amount for each violation type?
 
 SELECT Violation_Type, AVG(Fine_Amount) avg_fine
 FROM indian_traffic_violations1
