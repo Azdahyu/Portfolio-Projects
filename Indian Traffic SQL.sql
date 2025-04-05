0fa93c7ceba2377a95e86e89c5be2362a44ad70b
Okolie Augustina Chioma <138698620+Azdahyu@users.noreply.github.com>
2025-04-05 01:26:23 +0100

    Update Indian Traffic SQL.sql

--- Indian Traffic SQL.sql
+++ Indian Traffic SQL.sql
@@ -1,6 +1,5 @@
 /*
-Exploratory Data Analysis (EDA) in MySQL
-This dataset was gotten from https://www.kaggle.com/datasets/khushikyad001/indian-traffic-violation
+Exploratory Data Analysis (EDA) in MySQL Workbench
 */
 
 -------------------------------------------------------------------------------------------------------------------
